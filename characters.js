2a579a7b4e9695cca33870716d6695e6a7a13386
benjamin-schultz <41674946+benjamin-schultz@users.noreply.github.com>
2021-06-27 15:49:24 +1000

    more questions
    Added more questions

--- characters.js
+++ characters.js
@@ -1,92 +1,182 @@
 characters = {  
     sol: {
+        name: "Sol Badguy",
+        playstyle: "prepared",
         moral_system: "grey",
         gender: "male",
         favourite: ["pressure"],
+        important: ["all_rounder", "damage"],
+        zoning: 0.0,
+        rushdown: 1.0,
+        oneplayer: "sometimes",
         score: 0
     },
     ky: {
+        name: "Ky Kiske",
+        playstyle: "prepared",
         moral_system: "good",
         gender: "male",
         favourite: ["neutral", "pressure"],
+        important: ["all_rounder"],
+        zoning: 0.5,
+        rushdown: 0.5,
+        oneplayer: "no",
         score: 0
     },
     May: {
+        name: "May",
+        playstyle: "rewarding",
         moral_system: "good",
         gender: "female",
         favourite: ["neutral", "combos"],
+        important: ["mobility", "damage"],
+        zoning: 0.25,
+        rushdown: 0.75,
+        oneplayer: "no",
         score: 0
     },
     faust: {
+        name: "Faust",
+        playstyle: "specialist",
         moral_system: "neutral",
         gender: "male",
         favourite: ["neutral"],
+        important: ["utility"],
+        zoning: 1.0,
+        rushdown: 0.0,
+        oneplayer: "no",
         score: 0
     },
     millia: {
+        name: "Millia Rage",
+        playstyle: "specialist",
         moral_system: "grey",
         gender: "female",
         favourite: ["mindgames", "neutral"],
+        important: ["mobility", "utility"],
+        zoning: 0.0,
+        rushdown: 1.0,
+        oneplayer: "yes",
         score: 0
     },
     chipp: {
+        name: "Chipp Zanuff",
+        playstyle: "prepared",
         moral_system: "good",
         gender: "male",
         favourite: ["pressure", "neutral"],
+        important: ["mobility", "utility"],
+        zoning: 0.0,
+        rushdown: 1.0,
+        oneplayer: "sometimes",
         score: 0
     },
     ino: {
-        moral_system: "villian",
+        name: "I-No",
+        playstyle: "rewarding",
+        moral_system: "evil",
         gender: "female",
         favourite: ["mindgames"],
+        important: ["mobility"],
+        zoning: 0.0,
+        rushdown: 1.0,
+        oneplayer: "yes",
         score: 0
     },
     axl: {
+        name: "Axl Low",
+        playstyle: "specialist",
         moral_system: "neutral",
         gender: "male",
         favourite: ["neutral", "pressure"],
+        important: ["all_rounder"],
+        zoning: 1.0,
+        rushdown: 0.0,
+        oneplayer: "no",
         score: 0
     },
     zato: {
+        name: "Zato-1",
+        playstyle: "rewarding",
         moral_system: "neutral",
         gender: "male",
         favourite: ["combos", "mindgames"],
+        important: ["utility", "damage"],
+        zoning: 0.25,
+        rushdown: 0.75,
+        oneplayer: "yes",
         score: 0
     },
     pot: {
+        name: "Potemkin",
+        playstyle: "rewarding",
         moral_system: "neutral",
         gender: "male",
         favourite: ["pressure"],
+        important: ["health", "damage"],
+        zoning: 0.5,
+        rushdown: 0.5,
+        oneplayer: "no",
         score: 0
     },
     ramlethal: {
+        name: "Ramlethal Valentine",
+        playstyle: "rewarding",
         moral_system: "neutral",
         gender: "female",
         favourite: ["combos", "pressure"],
+        important: ["all_rounder"],
+        zoning: 0.75,
+        rushdown: 0.25,
+        oneplayer: "no",
         score: 0
     },
     leo: {
+        name: "Leo Whitefang",
+        playstyle: "rewarding",
         moral_system: "good",
         gender: "male",
         favourite: ["pressure", "mindgames"],
+        important: ["damage", "health"],
+        zoning: 0.0,
+        rushdown: 1.0,
+        oneplayer: "sometimes",
         score: 0
     },
     anji: {
+        name: "Anji Mito",
+        playstyle: "specialist",
         moral_system: "neutral",
         gender: "male",
         favourite: ["pressure", "neutral"],
+        important: ["all_rounder"],
+        zoning: 0.25,
+        rushdown: 0.75,
+        oneplayer: "no",
         score: 0
     },
     nago: {
-        moral_system: "villian",
+        name: "Nagoriyuki",
+        playstyle: "specialist",
+        moral_system: "evil",
         gender: "male",
         favourite: ["pressure", "combos"],
+        important: ["damage", "health"],
+        zoning: 0.75,
+        rushdown: 0.25,
+        oneplayer: "no",
         score: 0
     },
     giovanna: {
+        name: "Giovanna",
+        playstyle: "prepared",
         moral_system: "neutral",
         gender: "female",
         favourite: ["pressure"],
+        important: ["mobility", "all_rounder"],
+        zoning: 0.0,
+        rushdown: 1.0,
+        oneplayer: "sometimes",
         score: 0
     }
 
@@ -100,8 +190,20 @@ function reset_characters() {
 }
 function check_characters(result) {
     for (var char in characters) {
-        if (characters[char][result.key].includes(result.id) || result.id === "none") {
-            characters[char].score += result.amount;
+        if (typeof(characters[char][result.key]) === "number") {
+            check_numbers(char, result);
+        } else {
+            check_strings(char, result);
         }
     }
+}
+
+function check_strings(char, result) {
+    if (characters[char][result.key].includes(result.id) || result.id === "none") {
+        characters[char].score += result.amount;
+    }
+}
+
+function check_numbers(char, result) {
+    characters[char].score += result.amount * characters[char][result.key];
 }
--- questions.js
+++ questions.js
@@ -20,43 +20,11 @@ function get_results() {
     var questions = document.querySelectorAll('input[type="radio"]:checked');
     for (i = 0; i < questions.length; i++) {
         result = new Result(questions[i].name, questions[i].id);
-        switch(questions[i].name) {
-            case "moral_system":
-                calc_morals(result);
-                break;
-            case "gender":
-                calc_gender(result);
-                break;
-            case "favourite":
-                calc_favourite(result);
-                break;
-
-        }
+        result.check_strength();
+        check_characters(result);
     }
 
     document.getElementById("results_label").innerHTML = characters.chipp.score + " " + characters.faust.score;
 };
-
-function calc_morals(result) {
-    if (result.id == "any_good" || result.id == "chaotic_good" || result.id == "pure_good") {
-        result.id = "good";
-    }
-    check_characters(result); 
-}
-
-function calc_gender(result) {
-    result.check_strength();
-    
-    check_characters(result);
-}
-
-function calc_favourite(result) {
-    check_characters(result);
-}
-
-function calc_important(result) {
-    check_characters(result);
-}
-
 
 
